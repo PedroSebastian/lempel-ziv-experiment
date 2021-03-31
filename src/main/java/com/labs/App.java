7d3d3459586c868858a6a17a108a614586bcc9fb
Pedro Sebastian <pedrosebastian90@gmail.com>
2021-03-30 23:56:19 -0300

    Remove dependências desnecessárias

--- src/main/java/com/labs/App.java
+++ src/main/java/com/labs/App.java
@@ -1,28 +1,28 @@
 package com.labs;
 
 import com.sauljohnson.lizard.LzwCompressor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-public class App
-{
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
-
+public class App {
     /**
      * Application entry point.
      *
      * @param args application command line arguments
      */
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         try {
+            final long reference = System.nanoTime();
+
             LzwCompressor lzwCompressor = new LzwCompressor();
             lzwCompressor.compress("Hello World".getBytes(StandardCharsets.UTF_8));
+
+            final long finish = System.nanoTime();
+            System.out.println(((double) (finish - reference)) / 1000000000.0);  // In seconds
         } catch (IOException exception) {
-            logger.error(exception.getMessage());
+            System.out.println(exception.getMessage());
         }
     }
 }
