b7886c056aa0ed0576e7864548c8a4d7f3356677
Pedro Sebastian Zanella Nuñez <pedro.sebastian@evoluum.com.br>
2021-04-08 14:59:36 -0300

    Prossegue com implementação da leitura do arquivo

--- src/main/java/com/labs/App.java
+++ src/main/java/com/labs/App.java
@@ -1,12 +1,15 @@
 package com.labs;
 
 import com.sauljohnson.lizard.LzwCompressor;
+import org.apache.commons.cli.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class App {
     private static final Logger logger = LogManager.getLogger(App.class);
@@ -18,6 +21,23 @@ public class App {
      */
     public static void main(String[] args) {
         try {
+            final Options options = new Options();
+            options.addOption(new Option("f", "file", true, "Select file."));
+
+            final CommandLineParser parser = new DefaultParser();
+            final CommandLine commandLine = parser.parse(options, args);
+
+            byte[] file;
+
+            if (commandLine.hasOption('f')) {
+                final String fileName = commandLine.getOptionValue("file");
+
+                file = Files.readAllBytes(Paths.get(fileName));
+                System.out.println(file.toString());
+            } else {
+                throw new IllegalArgumentException("Invalid argument");
+            }
+
             final long reference = System.nanoTime();
 
             LzwCompressor lzwCompressor = new LzwCompressor();
@@ -25,7 +45,7 @@ public class App {
 
             final long finish = System.nanoTime();
             logger.info(((double) (finish - reference)) / 1000000000.0);  // In seconds
-        } catch (IOException exception) {
+        } catch (IOException | ParseException | IllegalArgumentException exception) {
             logger.error(exception.getMessage());
         }
     }
